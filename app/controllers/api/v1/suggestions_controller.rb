9ac938ce69bb219ad2c94d0cc1d2a2a969591423
IsuruV <isuru260@gmail.com>
2017-06-13 23:53:05 +0000

    auth issue fixed

--- app/controllers/api/v1/suggestions_controller.rb
+++ app/controllers/api/v1/suggestions_controller.rb
@@ -1,7 +1,7 @@
 module Api::V1
     class SuggestionsController < ApiController
         def create
-            suggestion = Suggestion.create
+            # suggestion = Suggestion.create
         end
     
     private
--- app/controllers/api/v1/tests_controller.rb
+++ app/controllers/api/v1/tests_controller.rb
@@ -1,12 +1,16 @@
   module Api::V1
+    
   class TestsController < ApiController
-
+    before_filter :authenticate_user!
     def index
         render json: 'works'
     end
     
     def user
-      render json: User.all.first, status: 200
+      user = current_user
+      render json: user.to_json, status: 200
     end
-end
+    
+  end
+  
 end
